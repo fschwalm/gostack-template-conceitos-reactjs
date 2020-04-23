407aaee35c7dd4a006f2e78d6a7a3929f18efe3b
fschwalm <fagnerschwalm@gmail.com>
2020-04-23 19:18:15 -0300

    finish challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
+import api from "./services/api";
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function loadRepositories() {
+      try {
+        const response = await api.get("/repositories");
+        setRepositories(response.data);
+      } catch (error) {
+        throw error;
+      }
+    }
+    loadRepositories();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const response = await api.post("/repositories", {
+        title: `Fagner ${getRandomInt(0, 100)}`,
+        url: "https://github.com/fschwalm",
+        techs: ["Node", "Express", "TypeScript"],
+      });
+      setRepositories((prevState) => [...prevState, response.data]);
+    } catch (error) {
+      throw error;
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+      setRepositories((prevState) =>
+        prevState.filter((repository) => repository.id !== id)
+      );
+    } catch (error) {
+      throw error;
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(({ id, title }) => (
+          <li key={id}>
+            {title}
+            <button onClick={() => handleRemoveRepository(id)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
